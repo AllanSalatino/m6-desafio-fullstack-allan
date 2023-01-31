fd33d0baf66eef3658b702bac818541fb679715b
Allan da Mota Salatino <allan.smota@gmail.com>
2023-01-31 18:42:19 -0300

    Entity, interface, controller, service e route foram criados

---
+++ back-end/src/services/contact/createContact.service.ts
@@ -0,0 +1,49 @@
+import AppDataSource from "../../data-source";
+import { Contact } from "../../entities/contact.entity";
+import { AppError } from "../../errors";
+import { IContactRequest } from "../../interfaces/contact";
+
+const createContactService = async (
+  { name, email, telephone }: IContactRequest,
+  id: string
+) => {
+  if (!name) {
+    throw new AppError("name is a field required");
+  }
+
+  if (!email) {
+    throw new AppError("email is a field required");
+  }
+
+  if (!telephone) {
+    throw new AppError("telephone is a field required");
+  }
+
+  const contactRepository = AppDataSource.getRepository(Contact);
+  const emailAlreadyExists = await contactRepository.findOneBy({
+    email: email,
+  });
+  const telephoneAlredyExists = await contactRepository.findOneBy({
+    telephone: telephone,
+  });
+
+  if (emailAlreadyExists) {
+    throw new AppError("Email already exists", 400);
+  } else if (telephoneAlredyExists) {
+    throw new AppError("Telephone already exists", 400);
+  }
+
+  const contact = contactRepository.create({
+    clientId: id,
+    name,
+    email,
+    telephone,
+    registration_date: new Date(),
+  });
+
+  await contactRepository.save(contact);
+
+  return contact;
+};
+
+export default createContactService;
